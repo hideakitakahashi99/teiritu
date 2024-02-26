d2fccf9fe40ea9d0b04a9f22541a952f613f946f
TakahashiHideaki <takahashihideaki@TakahashiHideakinoMacBook-Pro.local>
2024-02-26 16:31:25 +0900

    domain

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,6 @@ module Teiritu
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = 'Tokyo'
-    config.hosts << 'nora-college.onrender.com'
+    config.hosts << 'www.nora-college.com'
   end
 end
